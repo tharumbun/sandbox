607eac6d1f32c53121323f8e69a46d63676c699c
kokitree <hello@kokitree.com>
2025-03-23 22:33:26 +0700

    gitedit

--- src/config.ts
+++ src/config.ts
@@ -11,10 +11,5 @@ export const SITE = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
-  editPost: {
-    url: "https://github.com/satnaing/astro-paper/edit/main/src/content/blog",
-    text: "Suggest Changes",
-    appendFilePath: true,
-  },
   dynamicOgImage: true,
 } as const;
