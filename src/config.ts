134fdb4aa8b3b43bf6b30e4d5066c28632352ac4
kokitree <94977757+tharumbun@users.noreply.github.com>
2025-05-14 21:52:53 +0700

    update

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://tharum.com/", // replace this with your deployed domain
+  website: "https://tharum.com", // replace this with your deployed domain
   author: "Bun Tharum",
-  profile: "https://tharum.com/",
-  desc: "musings from the heart of cambodia",
-  title: "Tharum",
+  profile: "https://tharum.com",
+  desc: "Blogging from the heart of Cambodia",
+  title: "Tharum: Musings from Cambodia",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -14,7 +14,7 @@ export const SITE = {
   editPost: {
     url: "https://github.com/satnaing/astro-paper/edit/main/src/content/blog",
     text: "Suggest Changes",
-    appendFilePath: true,
+    appendFilePath: false,
   },
   dynamicOgImage: true,
 } as const;
